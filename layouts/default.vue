c3c12f4a95cf6f3ee52a802d66aaeb6c31da7b60
Your Name <you@example.com>
2020-11-07 00:58:24 +0200

    adding files

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+.default
+  NavigateBar.default__navigate-bar
+  nuxt.default__content
+  Footer.default__footer
+</template>
+
+<script>
+import NavigateBar from '~/components/NavBar'
+import Footer from '~/components/Footer'
+export default {
+  name: 'default',
+  components: {
+    NavigateBar,
+    Footer
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.default {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  padding-top: 43px;
+  &__navigate-bar {}
+  &__content {}
+  &__footer {
+    margin-top: auto;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+.main-page
+  .container
+    h1 Massage salon
+</template>
+
+<script>
+export default {
+  name: 'main-page'
+}
+</script>
+
+<style></style>
